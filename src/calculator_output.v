03cfaf5086496f5e1b135fc32b307020631c0b57
Gendronja <42985401+Gendronja@users.noreply.github.com>
2020-11-03 20:49:35 -0800

    Made those 0's BEAUTIFUL (fixed their logic)

--- src/calculator_output.v
+++ src/calculator_output.v
@@ -14,6 +14,7 @@ module calculator_output(
 	reg block_fill;
 	reg digit;
 	wire [9:0] arrayPos;
+	wire [3:0] row, col;
 	
 	//these two values dictate the center of the block, incrementing and decrementing them leads the block to move in certain directions
 	
@@ -80,7 +81,7 @@ module calculator_output(
 		// if in pixel 4 or 5 of a text block row, and digit is "1", fill block
 		   if (digit == 1)
 		   begin
-			   case (hCount % 10)
+			   case (col)
 			
 			    10'd0:
 			    	block_fill <= 0;
@@ -95,10 +96,12 @@ module calculator_output(
 				    block_fill <= 0;
 				
 			    10'd4:
-				    block_fill <= 1;
+			        if((row != 0) && (row != 9))
+				        block_fill <= 1;
 			
 			    10'd5:
-				    block_fill <= 1;
+			        if((row != 0) && (row != 9))
+				        block_fill <= 1;
 			
 			    10'd6:
 				    block_fill <= 0;
@@ -118,59 +121,59 @@ module calculator_output(
 			// Logic for writing a 0
 		    else if (digit == 0)
 		    begin
-			    case (hCount % 10)
+			    case (col)
 			
 			    10'd0:
 				    block_fill <= 0;
 			
 			    10'd1:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 3) || (row == 4) || (row == 5) || (row == 6))
 						block_fill <= 1;
 					else
 						block_fill <= 0;
 			
 			    10'd2:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 3) || (row == 4) || (row == 5) || (row == 6))
 						block_fill <= 1;
 					else
 						block_fill <= 0;
 				
 			    10'd3:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 1) || (row == 2) || (row == 7) || (row == 8))
 						block_fill <= 1;
 						
 					else
 						block_fill <= 0;
 				
 			    10'd4:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 1) || (row == 2) || (row == 7) || (row == 8))
 						block_fill <= 1;
 						
 					else
 						block_fill <= 0;
 			
 			    10'd5:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 1) || (row % 10 == 2) || (row == 7) || (row == 8))
 						block_fill <= 1;
 						
 					else
 						block_fill <= 0;
 			
 			    10'd6:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 1) || (row == 2) || (row == 7) || (row == 8))
 						block_fill <= 1;
 						
 					else
 						block_fill <= 0;
 			
 			    10'd7:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 3) || (row == 4) || (row == 5) || (row == 6))
 						block_fill <= 1;
 					else
 						block_fill <= 0;
 				
 			    10'd8:
-					if ((vCount % 10 == 3) || (vCount % 10 == 4) || (vCount % 10 == 5) || (vCount % 10 == 6))
+					if ((row == 3) || (row == 4) || (row == 5) || (row == 6))
 						block_fill <= 1;
 					else
 						block_fill <= 0;
@@ -192,8 +195,9 @@ module calculator_output(
 	// Pick array value based on hCount and vCount, only usable in specific A, B, C blocks. Assign 0 if outside of blocks
 	assign arrayPos = ((vCount >= AVert) && (vCount <= DVert) && (hCount >= hStartPos) && (hCount <= hEndPos)) ? 
 					  ((((hCount % 100) - (hCount % 10)))/10 + (10*(hCount >= 300))) : 0;
-	//assign col = (hCount % 10);
-	//assign row = (vCount % 10);
+					  
+	assign col = (hCount % 10);
+	assign row = (vCount % 10);
 	//assign textBlock = ((vCount >= AVert) && (vCount <= BVert) && (hCount >= 250) && (hCount <= 400));
 	//assign block_fill = ((digit == 1)&&(  ) ? ((vCount % 10) ) : ( )
 	
